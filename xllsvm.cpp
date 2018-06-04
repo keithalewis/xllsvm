6a5ada81454be7d2a7e3c041ebafbb451074444d
Keith A. Lewis <keithalewis@live.com>
2018-06-04 18:47:00 -0400

    sync

---
+++ xllsvm.cpp
@@ -0,0 +1,33 @@
+// xlltemplate.cpp
+#include <cmath>
+#include "xllsvm.h"
+
+using namespace xll;
+
+//enum { C_SVC, NU_SVC, ONE_CLASS, EPSILON_SVR, NU_SVR };	/* svm_type */
+XLL_ENUM_DOC(C_SVC, SVM_C_SVC, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(NU_SVC, SVM_NU_SVC, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(ONE_CLASS, SVM_ONE_CLASS, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(EPSILON_SVR, SVM_EPSILON_SVR, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(NU_SVR, SVM_NU_SVR, CATEGORY, L"description", L"documentation");
+
+//enum { LINEAR, POLY, RBF, SIGMOID, PRECOMPUTED }; /* kernel_type */
+XLL_ENUM_DOC(LINEAR, KERNEL_LINEAR, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(POLY, KERNEL_POLY, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(RBF, KERNEL_RBF, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(SIGMOID, KERNEL_SIGMOID, CATEGORY, L"description", L"documentation");
+XLL_ENUM_DOC(PRECOMPUTED, KERNEL_PRECOMPUTED, CATEGORY, L"description", L"documentation");
+
+
+// Information Excel needs to register add-in.
+AddIn xai_function(
+    Function(XLL_DOUBLE, L"?xll_function", L"XLL.FUNCTION")
+    .Arg(XLL_DOUBLE, L"x", L"is the first double argument.")
+    .Category(CATEGORY)
+    .FunctionHelp(L"Help on XLL.FUNCTION goes here.")
+);
+double WINAPI xll_function(double x)
+{
+#pragma XLLEXPORT
+    return exp(x);
+}
---
+++ xllsvm.h
@@ -0,0 +1,39 @@
+// xllsvm.h - Support vector machine add-in.
+#include "libsvm/svm.h"
+#include "xll12/xll/xll.h"
+
+// Function Wizard category
+#ifndef CATEGORY
+#define CATEGORY L"SVM"
+#endif
+
+namespace svm {
+
+    struct parameter : public svm_parameter {
+        //parameter()
+        //    : svm_parameter{ C_SVC, RBF, 3, 0, 0, 100, 1e-3, 1, 0, nullptr, nullptr, 0.5, 0.1, 1, 0 }
+        //{ }
+        parameter(
+            int svm_type = C_SVC,
+            int kernel_type = RBF,
+            int degree = 3,	/* for poly */
+            double gamma = 0,	/* for poly/rbf/sigmoid */
+            double coef0 = 0,	/* for poly/sigmoid */
+
+                            /* these are for training only */
+            double cache_size = 100, /* in MB */
+            double eps = 1e-3,	/* stopping criteria */
+            double C = 1,	/* for C_SVC, EPSILON_SVR and NU_SVR */
+            int nr_weight = 0,		/* for C_SVC */
+            int *weight_label = nullptr,	/* for C_SVC */
+            double* weight = nullptr,		/* for C_SVC */
+            double nu = 0.5,	/* for NU_SVC, ONE_CLASS, and NU_SVR */
+            double p = 0.1,	/* for EPSILON_SVR */
+            int shrinking = 1,	/* use the shrinking heuristics */
+            int probability = 0 /* do probability estimates */
+        )
+            : svm_parameter{svm_type, kernel_type, degree, gamma, coef0, cache_size, eps, C, nr_weight, weight_label, weight, nu, p, shrinking, probability}
+        { }
+    };
+
+} // namespace svm
